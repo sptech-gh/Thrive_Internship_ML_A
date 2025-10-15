e558babb8b4a5c3a29c63a048f14561de41f1bf8
shaddydevops <122651230+shaddydevops@users.noreply.github.com>
2025-10-15 20:36:02 +0000

    fixing ci and clearing all folders

--- src/modeling/model.py
+++
@@ -1,5 +0,0 @@
-
-from transformers import AutoModelForSequenceClassification
-
-def load_model(model_name_or_path: str, num_labels: int = 4):
-    return AutoModelForSequenceClassification.from_pretrained(model_name_or_path, num_labels=num_labels)
